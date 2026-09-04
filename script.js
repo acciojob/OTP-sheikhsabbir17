dea787aaf3d28eec237c4d7dadaf5f43f3450075
Sheikh Sabbir <144557992+sheikhsabbir17@users.noreply.github.com>
2026-09-04 20:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+  code.addEventListener("input", () => {
+    if (code.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (!code.value && index > 0) {
+        codes[index - 1].focus();
+      }
+    }
+  });
+});
